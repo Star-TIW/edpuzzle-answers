dccc5e931df39669d2140cf0c4dab9b4c92efd8e
Star <125516384+Star-TIW@users.noreply.github.com>
2025-11-24 17:04:11 -0800

    Update open.js

--- open.js
+++ open.js
@@ -5,26 +5,32 @@
 
 const gpl_text = `ading2210/edpuzzle-answers: a Javascript bookmarklet that provides many useful utilities for Edpuzzle
 Copyright (C) 2025 ading2210
-
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License along with this program.If not, see <https://www.gnu.org/licenses/>.`;
+...
+`;
 
 //legacy code - use the fetch api instead
 function http_get(url, callback, headers=[], method="GET", content=null) {
+  console.log(`[HTTP] Preparing ${method} request to: ${url}`);
+
   var request = new XMLHttpRequest();
-  request.addEventListener("load", callback);
+  request.addEventListener("load", function() {
+    console.log(`[HTTP] Response received from: ${url}`);
+    callback.call(this);
+  });
+
   request.open(method, url, true);
 
   if (window.__EDPUZZLE_DATA__ && window.__EDPUZZLE_DATA__.token && (new URL(url).hostname) == "edpuzzle.com") {
     headers.push(["authorization", window.__EDPUZZLE_DATA__.token]);
+    console.log(`[HTTP] Added authorization header for Edpuzzle`);
   }
+
   for (const header of headers) {
+    console.log(`[HTTP] Added header: ${header[0]} = ${header[1]}`);
     request.setRequestHeader(header[0], header[1]);
   }
   
+  console.log(`[HTTP] Sending request...`);
   request.send(content);
 }
 
@@ -41,9 +47,11 @@ function format_text(text, replacements) {
 function init() {
   console.info(gpl_text);
 
-  //support running from within ultraviolet
+  console.log(`[Init] Current URL: ${window.location.href}`);
+
   window.real_location = window.location;
   if (window.__uv) {
+    console.log("[Init] Running within Ultraviolet proxy");
     window.real_location = __uv.location;
   }
 
@@ -51,28 +59,34 @@ function init() {
     alert("To use this, drag this button into your bookmarks bar. Then, run it when you're on an Edpuzzle assignment.");
   }
   else if ((/https?:\/\/edpuzzle.com\/(lms\/lti\/)?assignments\/[a-f0-9]{1,30}\/(watch|view)/).test(window.real_location.href)) {
+    console.log("[Init] Detected Edpuzzle assignment page. Loading popup...");
     http_get(base_url+"/popup.html", open_popup);
   }
   else if (window.canvasReadyState) {
+    console.log("[Init] Detected Canvas environment");
     handle_canvas_url();
   }
   else if (window.schoologyMoreLess) {
+    console.log("[Init] Detected Schoology environment");
     handle_schoology_url();
   }
   else {
-    alert("Please run this script on an Edpuzzle assignment. For reference, the URL should look like this:\nhttps://edpuzzle.com/assignments/{ASSIGNMENT_ID}/watch");
+    console.log("[Init] Not an Edpuzzle assignment. Aborting.");
+    alert("Please run this script on an Edpuzzle assignment...");
   }
 }
 
 function open_popup() {
+  console.log("[Popup] Writing popup window with loaded HTML");
   const popup = window.open("about:blank", "", "width=760, height=450");
   if (popup == null) {
-    alert("Error: Could not open the popup. Please enable popups for edpuzzle.com and try again.");
+    alert("Error: Could not open the popup...");
     return;
   }
   write_popup(popup, this.responseText);
   
   function popup_unload() { 
+    console.log("[Popup] Reloading popup content after unload");
     http_get(base_url+"/popup.html", function(){
       if (popup.closed) return;
       write_popup(popup, this.responseText);
@@ -84,6 +98,8 @@ function open_popup() {
 }
 
 function write_popup(popup, html) {
+  console.log("[Popup] Injecting CSS, JS, and HTML into popup");
+
   popup.document.base_url = base_url;
   popup.document.edpuzzle_data = window.__EDPUZZLE_DATA__;
   popup.document.gpl_text = gpl_text;
@@ -97,38 +113,53 @@ function write_popup(popup, html) {
   }
 
   http_get(base_url+"/styles/popup.css", function(){
+    console.log("[Popup] Loaded popup CSS");
     create_element("style", this.responseText);
   });
 
   http_get(base_url+"/main.js", function() {
+    console.log("[Popup] Loaded popup JS");
     create_element("script", this.responseText);
   });
 }
 
 function handle_canvas_url() {
+  console.log("[Canvas] Starting Canvas assignment resolution");
+
   let location_split = window.real_location.href.split("/");
   let url = `/api/v1/courses/${location_split[4]}/assignments/${location_split[6]}`;
+  console.log(`[Canvas] Fetching assignment info from: ${url}`);
+
   http_get(url, function(){
     let data = JSON.parse(this.responseText);
     let url2 = data.url;
+    console.log(`[Canvas] Found external tool URL: ${url2}`);
 
     http_get(url2, function() {
       let data = JSON.parse(this.responseText);
       let url3 = data.url;
+      console.log(`[Canvas] Final Edpuzzle URL discovered: ${url3}`);
+      console.log("[Canvas] Opening final target tab...");
 
-      alert(`Please re-run this script in the newly opened tab. If nothing happens after pressing "ok", then allow popups on Canvas and try again.`);
+      alert(`Please re-run this script in the newly opened tab...`);
       open(url3);
     });
   });
 }
 
 function handle_schoology_url() {
+  console.log("[Schoology] Starting Schoology assignment resolution");
+
   let assignment_id = window.real_location.href.split("/")[4];
   let url = `/external_tool/${assignment_id}/launch/iframe`;
-  http_get(url, function() {
-    alert(`Please re-run this script in the newly opened tab. If nothing happens after pressing "ok", then allow popups on Schoology and try again.`);
 
-    //strip js tags from response and add to dom
+  console.log(`[Schoology] Fetching launch iframe from: ${url}`);
+
+  http_get(url, function() {
+    console.log("[Schoology] Received response. Processing form...");
+
+    alert(`Please re-run this script in the newly opened tab...`);
+
     let html = this.responseText.replace(/<script[\s\S]+?<\/script>/, ""); 
     let div = document.createElement("div");
     div.innerHTML = html;
@@ -139,9 +170,10 @@ function handle_schoology_url() {
     input.setAttribute("name", "ext_submit");
     input.setAttribute("value", "Submit");
     form.append(input);
-    document.body.append(div);
 
-    //submit form in new tab
+    console.log("[Schoology] Submitting external tool form in new tab");
+
+    document.body.append(div);
     form.setAttribute("target", "_blank");
     form.submit();
     div.remove();
